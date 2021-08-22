e669f7ae525d35651c51966fee316fbfb7067ef1
iroshan <iroshanav@gmail.com>
2021-08-22 18:34:10 +0530

    new file change2

--- ClassA.java
+++ ClassA.java
@@ -4,6 +4,10 @@ public class ClassA {
 
 	public static void main(String[] args) {
 		System.out.println("Hello WOrld");
+		
+		for(int i=0; i<10 ; i++) {
+			System.out.println("Hello World "+i);
+		}
 	}
 	
 }
